c6d6b5647584e092338bef63f58eb9ef8922dcab
node-webs <type.script@mail.com>
2025-05-24 17:16:14 +0900

    commit

--- code/index.ts
+++ code/index.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import cors from 'cors';
 import multer from 'multer';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
@@ -14,11 +15,25 @@ nunjucks.configure(path.join(__dirname, "views"), {
   express: app,
   watch: true,
 });
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:8080'],
+  methods: ['GET', 'POST'], // 허용할 HTTP 메서드
+  allowedHeaders: ['Content-Type', 'Authorization'], // 특정 헤더만 허용
+  credentials: true, // 쿠키와 같은 자격 증명 허용
+}));
 
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, '../public')))
 console.log('dirname :: ', __dirname);
 app.use('/upload', express.static(path.join(__dirname, '../upload')))
+// app.use('/call', (req: Request, res: Response, next: NextFunction) => {
+//     if (true) {
+//         // http://localhost:3000/call/image/cat_w_01.png
+//         express.static(__dirname + '/data-code')(req, res, next);
+//     } else {
+//         next();
+//     }
+// })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // extended: true 
